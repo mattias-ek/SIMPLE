f86596f3bb036db177ae991171fb5c2cf16dcf06
mattias-ek <mattias.ek@erdw.ethz.ch>
2024-09-25 10:36:57 +0200

    Moved nugridpy  import to functions

---
+++ docs/install.md
@@ -0,0 +1,9 @@
+# Installing SIMPLE
+
+Currently, SIMPLE is currently not on PyPi, but it can be installed using PIP using 
+
+
+
+```
+pip install git+https://github.com/mattias-ek/SIMPLE@master
+```
--- simple/ccsne/load.py
+++ simple/ccsne/load.py
@@ -1,5 +1,4 @@
 import simple.utils as utils
-from nugridpy import nugridse as mp
 import numpy as np
 import h5py
 import re
@@ -19,6 +18,7 @@ def AGB_test(data_dir):
     pt_3 = mp.se(data_dir + 'agb_surf_m3z2m3/', '96101.surf.h5', rewrite=True)
 
 def Ri18(data_dir, ref_isoabu, ref_isomass, divide_by_isomass = True):
+    from nugridpy import nugridse as mp
     # loading Ritter+18 model
     fol2mod = data_dir + 'R18/'
     # load instances of models
@@ -66,6 +66,7 @@ def Ri18(data_dir, ref_isoabu, ref_isomass, divide_by_isomass = True):
     return models
 
 def Pi16(data_dir, ref_isoabu, ref_isomass, divide_by_isomass = True):
+    from nugridpy import nugridse as mp
     # loading Pignatari+16 model
     fol2mod = data_dir + 'P16/'
     # load instances of models
