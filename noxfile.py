4b90a7c2095277858d8f4579e6a88d5759b08137
mattias-ek <mattias.ek@erdw.ethz.ch>
2025-07-08 12:04:05 +0200

    Updated github testing and coverage action

---
+++ noxfile.py
@@ -0,0 +1,23 @@
+import nox
+
+@nox.session(python=["3.9", "3.12"])
+@nox.parametrize(
+    ("numpy", "matplotlib", "scipy", "h5py", "pyyaml"),
+    [
+        ("1.22", "3.5", "1.8", "3.6", "5.4"),   # minimum versions
+        ("latest", "latest", "latest", "latest", "latest"),  # latest versions
+    ]
+)
+def test_matrix(session, numpy, matplotlib, scipy, h5py, pyyaml):
+    packages = dict(
+        numpy=numpy,
+        matplotlib=matplotlib,
+        scipy=scipy,
+        h5py=h5py,
+        pyyaml=pyyaml,
+    )
+    for package, version in packages.items():
+        session.install(package + "" if version == "latest" else f"=={version}")
+
+    session.install(".[test]")
+    session.run("pytest")
